92e49a731da0eea42ebc0bce0ebb3f21c58c777b
Nathan Ware <nware6184@sdsu.edu>
2021-02-19 15:54:54 -0800

    Built out menu logic for New Character, Main Menu, and Game Over Menu.  - New Character menu has functioning back button which should work modularly with other screens(with default back -> main menu.  - New Character menu has save button which for testing purposes Logs the name of the char being saved, and increases the num of saved characters    *In the future, num of saved characters should be determined dynamically at startup based on the num of character data files saved in the Characters Library  - Main Menu Play button now only activates when at least 1 character exists  - Game over menu returns to main menu

--- Assets/Scripts/GAMEMANAGER.cs
+++ Assets/Scripts/GAMEMANAGER.cs
@@ -2,20 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GAMEMANAGER : MonoBehaviour
 {
-    public static scene previousScene;
+    public static string previousScene = "Main";
+    public static int charactersCount = 0;
+    public Button playButton;
     // Start is called before the first frame update
+
     void Start()
     {
         Debug.Log("Game Manager - in start function.");
+        if(charactersCount == 0){
+            if(playButton){playButton.interactable = false;}
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(playButton){
+            if(playButton.interactable == false && charactersCount > 0){
+                playButton.interactable = true;
+            }
+        }
     }
 
     public void onExitClicked(){
@@ -30,14 +41,24 @@ public class GAMEMANAGER : MonoBehaviour
 
     public void onNewCharacterClicked(){
         Debug.Log("Creating new character.");
-        previousScene = Scene.name;
+        previousScene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("NewCharacter");
     }
 
     public void onBackButtonClicked(){
         
-        Debug.Log("Creating new character.");
-        previousScene = Scene.name;
+        Debug.Log("Returning to previous scene.");
         SceneManager.LoadScene(previousScene);
+        previousScene = SceneManager.GetActiveScene().name;
+    }
+
+    public void onPlayButtonClicked(){
+
+    }
+    
+    public void saveNewCharacter(InputField CharNameInputField){
+        string charName = CharNameInputField.text;
+        Debug.Log($"Saving new character named {charName}");
+        charactersCount++;
     }
 }
